1d8931472e7489fa4db164ac80e577c0e5f40c70
edouard <edouard.blais@hotmail.com>
2022-11-15 22:47:01 -0500

    fixed bug

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ http.createServer(function (req, res) {
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
+      return res.end(`<h1>Error: 404</h1><p>The page you are looking for doesn't exist.</p> <a href="/">Back home.</a>`);
     } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
